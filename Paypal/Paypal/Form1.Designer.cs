9eb4eb2376c125ff7b42998f135c7efafea39295
Yannick Gibson <yannick.gibson@seznam.cz>
2020-10-23 23:42:10 +0200

    update plz

--- Paypal/Paypal/Form1.Designer.cs
+++ Paypal/Paypal/Form1.Designer.cs
@@ -28,34 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.dayTimer = new System.Windows.Forms.Timer(this.components);
+            this.basicAccountLabel = new System.Windows.Forms.Label();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.basicAccountPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.studentsAccountLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.creditAccountLabel = new System.Windows.Forms.Label();
+            this.bottomPanel.SuspendLayout();
+            this.basicAccountPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label2
+            // dateLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(402, 138);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 15);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "hello";
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(17, 13);
+            this.dateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(48, 20);
+            this.dateLabel.TabIndex = 0;
+            this.dateLabel.Text = "Date: ";
+            // 
+            // dayTimer
+            // 
+            this.dayTimer.Enabled = true;
+            this.dayTimer.Interval = 1000;
+            this.dayTimer.Tick += new System.EventHandler(this.dayTimer_Tick);
+            // 
+            // basicAccountLabel
+            // 
+            this.basicAccountLabel.AutoSize = true;
+            this.basicAccountLabel.Location = new System.Drawing.Point(78, 49);
+            this.basicAccountLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.basicAccountLabel.Name = "basicAccountLabel";
+            this.basicAccountLabel.Size = new System.Drawing.Size(97, 20);
+            this.basicAccountLabel.TabIndex = 0;
+            this.basicAccountLabel.Text = "BasicAccount";
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.BackColor = System.Drawing.Color.SkyBlue;
+            this.bottomPanel.Controls.Add(this.dateLabel);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 560);
+            this.bottomPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Size = new System.Drawing.Size(1140, 40);
+            this.bottomPanel.TabIndex = 1;
+            // 
+            // basicAccountPanel
+            // 
+            this.basicAccountPanel.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.basicAccountPanel.Controls.Add(this.basicAccountLabel);
+            this.basicAccountPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.basicAccountPanel.Location = new System.Drawing.Point(0, 0);
+            this.basicAccountPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.basicAccountPanel.Name = "basicAccountPanel";
+            this.basicAccountPanel.Size = new System.Drawing.Size(240, 560);
+            this.basicAccountPanel.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.studentsAccountLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(240, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 560);
+            this.panel1.TabIndex = 2;
+            // 
+            // studentsAccountLabel
+            // 
+            this.studentsAccountLabel.AutoSize = true;
+            this.studentsAccountLabel.Location = new System.Drawing.Point(255, 49);
+            this.studentsAccountLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.studentsAccountLabel.Name = "studentsAccountLabel";
+            this.studentsAccountLabel.Size = new System.Drawing.Size(120, 20);
+            this.studentsAccountLabel.TabIndex = 0;
+            this.studentsAccountLabel.Text = "StudentsAccount";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel2.Controls.Add(this.creditAccountLabel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(900, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(240, 560);
+            this.panel2.TabIndex = 2;
+            // 
+            // creditAccountLabel
+            // 
+            this.creditAccountLabel.AutoSize = true;
+            this.creditAccountLabel.Location = new System.Drawing.Point(66, 49);
+            this.creditAccountLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.creditAccountLabel.Name = "creditAccountLabel";
+            this.creditAccountLabel.Size = new System.Drawing.Size(103, 20);
+            this.creditAccountLabel.TabIndex = 0;
+            this.creditAccountLabel.Text = "CreditAccount";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1140, 600);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.basicAccountPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "PayBank";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bottomPanel.ResumeLayout(false);
+            this.bottomPanel.PerformLayout();
+            this.basicAccountPanel.ResumeLayout(false);
+            this.basicAccountPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Timer dayTimer;
+        private System.Windows.Forms.Label basicAccountLabel;
+        private System.Windows.Forms.Panel bottomPanel;
+        private System.Windows.Forms.Panel basicAccountPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label studentsAccountLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label creditAccountLabel;
     }
 }
 
--- Paypal/Paypal/Form1.cs
+++ Paypal/Paypal/Form1.cs
@@ -12,14 +12,75 @@ namespace Paypal
 {
     public partial class Form1 : Form
     {
+        /*
+Příklad pro dvojčlenné skupiny:
+Vytvořte konzolový nebo WPF program, který bude simulovat funkce banky.
+Banka vede svým klientům účty, které jsou buď depozitní (spořicí) nebo kreditní (úvěrové).
+
+Na spořicí účty klienti vkládají své peníze za účelem zhodnocení(to je vyjádřené úrokovou sazbou spořicího účtu) a mohou z nich vybírat, jen pokud je jejich zůstatek kladný.
+Spořicí účet může mít svou variantu pro studenty. Ten má omezenou velikost jednorázového výběru, jinak se chová stejně, jako obyčejný spořicí účet.
+Úroky z vkladů banka svým klientům připisuje na konci měsíce.
+Z úvěrových účtů mohou klienti vybírat peníze do výše úvěrového rámce a s úročením, které určí banka (tedy mohou jít "do mínusu"). Vkládáním peněz na úvěrový účet splácí svůj dluh(jistinu a úroky) bance.
+
+Na všechny účty je možné vkládat a vybírat z nich, pokud to aktuální stav účtu nebo úvěrový limit dovoluje.
+
+Navrhněte třídy pro depozitní, studentský a úvěrový účet. Atributy tříd implementujte jako vlastnosti, rozvažte jejich dostupnost z ostatních tříd.
+Některé funkce(např. připisování úroků na konci měsíce) jsou závislé na čase. Navrhněte mechanismus pro testování těchto funkcí.
+
+V metodě Main() vytvořte instance depozitních, úvěrových a studentských účtů. Vytvořte pole těchto účtů, proveďte na nich vklady a výběry a nakonec vypište aktuální stav těchto účtů.
+Vytvořte projekt, který bude zahrnovat simulátor banky.
+Promyslete si dělbu práce na projektu a vytvořte dokument, který bude fungovat jako pracovní plán projektu. Buďte připraveni na jeho změny, dokument verzujte společně se zdrojovým kódem.
+Projekt verzujte na GitHubu a dohodněte se ve skupině, kdo bude za společné úložiště zodpovědný jako administrátor. 
+Nastudujte si principy spolupráce v GitHubu a spolupráci na společné repository si nejdříve vyzkoušejte na cvičném projektu.
+        */
+        public abstract class BankAccount
+        {
+            public decimal balance { get; set; }
+            //
+            public BankAccount(decimal startBalance = 10_000)
+            {
+                balance = startBalance;
+            }
+        }
+        public class SavingsAccount : BankAccount 
+        { 
+            public SavingsAccount() : base()
+            {
+    
+            }
+        }
+        public class StudentsSavingsAccount : SavingsAccount
+        {
+            public StudentsSavingsAccount() : base()
+            {
+
+            }
+        }
+        public class CreditAccount : BankAccount
+        {
+            public CreditAccount() : base()
+            {
+
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        DateTime date;
+        private void Form1_Load(object sender, EventArgs e)
         {
+            date = DateTime.Now;
+        }
 
+        private void dayTimer_Tick(object sender, EventArgs e)
+        {
+            dateLabel.Text = $"Day: {date.Day}. {date.Month} {date.Year}";
+            if(date.Day == 1)
+            {
+
+            }
         }
     }
 }
